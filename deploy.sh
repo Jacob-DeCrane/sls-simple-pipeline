ebab755576fbcc00edac43ecc37e18b6a11d5dfa
Jacob DeCrane <Jacob.Decrane@ge.com>
2019-04-08 14:13:28 -0400

    added codedeploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 echo "Installing serverless"
 echo "====================="
 npm install -g serverless
-npm install serverless-stage-manager serverless-pseudo-parameters # we have to install plugins specified in our serverless.yml to avoid errors, even though we aren't using them.
+# we have to install plugins specified in our serverless.yml to avoid errors, even though we aren't using them.
+npm install serverless-stage-manager serverless-pseudo-parameters serverless-plugin-canary-deployments
 
 echo "Deploying app to $env"
 echo "====================="
