99e1e5d64e661783e05c810a2b9c5229dfd6a5da
Jacob DeCrane <Jacob.Decrane@ge.com>
2019-04-08 11:19:35 -0400

    added npm install

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 echo "Installing serverless"
 echo "====================="
 npm install -g serverless
+npm install # we have to install plugins specified in our serverless.yml to avoid errors, even though we aren't using them.
 
 echo "Deploying app to $env"
 echo "====================="
